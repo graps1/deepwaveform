71976e6cc9c0b8a0197688174fd3cc23ddfdd4ff
Hans Harder <asdfsolider@googlemail.com>
2020-09-22 12:22:17 +0200

    renamed files

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="kplx_hansharder",
+    name="deepwaveform",
     version="0.1",
     packages=find_packages(),
 
     author="Hans Harder",
-    author_email="hans.harder@mailbox.tu-dresden.de"
+    author_email="hans.harder@mailbox.tu-dresden.de",
+
+    install_requires=[
+        "matplotlib",
+        "torch",
+        "numpy"
+    ]
 )
