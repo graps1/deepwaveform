2c9a4cf3070b82e507a0127960110825e039826c
Hans Harder <asdfsolider@googlemail.com>
2020-09-22 12:15:45 +0200

    update gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="kplx_hansharder",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Hans Harder",
+    author_email="hans.harder@mailbox.tu-dresden.de"
+)
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from .preprocessing import waveform2matrix, load_dataset
+from .model import ConvNet
+from .visualization import plot_pcl, plot_waveforms
